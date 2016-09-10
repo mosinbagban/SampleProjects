996a3d6bc44094b60bdf19bc0796367f10bbd1e7
Rahul Sahay <rahul.sahay@voya.com>
2016-09-10 00:33:09 -0400

    chat

--- src/app.js
+++ src/app.js
@@ -2,19 +2,36 @@
  * Created by n689716 on 9/7/16.
  */
 
-import {inject} from 'aurelia-framework';
-import {Router} from 'aurelia-router';
-import AppRouterConfig from './router-config';
+import {inject, BindingEngine} from 'aurelia-framework';
+//import {Router} from 'aurelia-router';
+//import AppRouterConfig from './router-config';
 
-@inject(Router, AppRouterConfig)
-export class Chat {
+@inject(BindingEngine)
+export class App {
 
-  constructor(router, appRouterConfig){
-    this.router = router;
-    this.appRouterConfig = appRouterConfig;
+  constructor(bindingEngine){
+    this.bindingEngine = bindingEngine;
+    //this.appRouterConfig = appRouterConfig;
   };
 
   activate() {
-    this.appRouterConfig.configure();
+    //this.appRouterConfig.configure();
   };
+
+  configureRouter(config, router) {
+    //debugger;
+    this.router = router;
+
+    config.title = 'Chat Application';
+
+    // Here, we describe the routes we want along with information about them
+    // such as which they are accessible at, which module they use, and whether
+    // they should be placed in the navigation bar
+    config.map([
+      { route: ['','welcome'], name: 'welcome', moduleId: './app/welcome-module/welcome'},
+      { route: 'chat', name: 'chat', moduleId: './app/chat-module/chat'}
+    ]);
+
+
+  }
 }
--- src/app/welcome-module/welcome.js
+++ src/app/welcome-module/welcome.js
@@ -14,11 +14,22 @@ export class Welcome {
         this.appConfig = appConfig;
 
         this.helpOptions = [];
+        this.disclaimerText = '';
+        this.genesysChatServiceAPI = '';
+
+        this.firstName = '';
+        this.lastName = '';
+        this.email = '';
+        this.sendTranscript = false;
+        this.helpMessage = '';
+        this.question = '';
     }
 
     created(owningView, view) {
 
         this.disclaimerText = this.appConfig.disclaimerText;
+
+        this.genesysChatServiceAPI = this.appConfig.genesysChatServiceAPI;
 
         this.appConfig.helpOptions.forEach(function(option){
             this.helpOptions.push({value:option.value, label:option.label})
@@ -27,8 +38,72 @@ export class Welcome {
     }
 
     startchat(){
-        //alert(`Hi`);
-        this.router.navigate("chat");
+        let firstName = this.firstName;
+        let lastName = this.lastName;
+        let email = this.email;
+        let sendTranscript = this.sendTranscript;
+        let helpMessage = this.helpMessage;
+        let question = this.question;
+
+        var widgetBus = window._genesys.cxwidget.bus;
+
+        window._genesys.cxwidget.bus.command("cx.plugin.WebChatService.configure", {
+            dataURL: this.genesysChatServiceAPI
+        }).done(function(e) {
+            // success scenario
+        }).fail(function(e) {
+            // failure scenario: error, exception, improper arguments
+            //alert('configure fail');
+        })
+
+
+        widgetBus.subscribe("cx.plugin.WebChat.ready", function(){
+            //alert('chat is ready');
+        });
+
+        widgetBus.subscribe("cx.plugin.WebChat.closed", function(){
+            //alert('chat is closed');
+            window.close();
+        });
+
+        widgetBus.subscribe("cx.plugin.WebChat.minimized", function(){
+           // alert('chat is minimized');
+        });
+
+        widgetBus.subscribe("cx.plugin.WebChat.unminimized", function(){
+            //alert('chat is unminimized');
+        });
+
+
+        window._genesys.cxwidget.bus.command("cx.plugin.WebChat.open", {form : false}).done(function(e)
+        {
+            $('.cx-widget.cx-webchat').find("textarea.input").removeClass("disabled").attr("disabled", false);
+            var myobject = $('.cx-widget.cx-webchat');
+
+            console.log('****************************'+$.find("textarea"));
+
+            window._genesys.cxwidget.bus.command("cx.plugin.WebChatService.startChat", {
+                userData: {"ClientID":"Honeywell-test","sendTranscript":sendTranscript , "vruApp":"vruApp-test" , "AuthenticationLevel":"test-pass" , "DNIS":"dnis-test" , "CUST_ID":"cust-id-test" ,"SSN":"ssn-test" },
+                nickname: firstName,
+                firstname: firstName,
+                lastname: lastName,
+                email: email,
+                subject: question
+            }).done(function(e) {
+                //alert('startChat Success');
+                // success scenario
+            }).fail(function(e) {
+                // failure scenario: error, exception, improper arguments
+                //alert('startChat Failure');
+                return false;
+            })// success scenario
+
+        }).fail(function(e)
+        {
+            // failure scenario: error, exception, improper arguments
+        })
+
+        //this.router.navigate("chat");
     }
 
 }
